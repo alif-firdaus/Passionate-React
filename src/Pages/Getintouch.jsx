0a2a9f8f1864e2074d0e5354a3d8f03f30391b5e
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-10-26 15:45:07 +0700

    Getintouch Section Light Mode : added

--- src/Pages/Getintouch.jsx
+++ src/Pages/Getintouch.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const Getintouch = () => {
 	return (
-		<div id="getintouch" className="pt-20 pb-56 h-fit bg-base px-sectionpl">
+		<div
+			id="getintouch"
+			className="pt-20 pb-56 h-fit dark:bg-base bg-lightgrey px-sectionpl"
+		>
 			{/* Get in Touch Title */}
-			<h1 className="text-5xl font-semibold text-center text-white">
+			<h1 className="text-5xl font-semibold text-center text-black dark:text-white">
 				Turning your ideas into{" "}
 				<span className="text-transparent bg-gradient-to-r from-primary via-purple-500 to-pink-500 bg-clip-text">
 					real-life
@@ -13,8 +16,8 @@ const Getintouch = () => {
 			</h1>
 
 			{/* Main Button */}
-			<div className="flex justify-center m-auto mt-20">
-				<button className="pt-3 pb-[15px] text-[17px] font-medium text-black bg-white rounded-xl w-fit h-fit px-10">
+			<div className="flex justify-center mt-14">
+				<button className="pt-3 pb-[15px] text-[17px] font-medium dark:text-black text-white dark:bg-white bg-black rounded-xl w-fit h-fit px-10">
 					Let's talk!
 				</button>
 			</div>
