fc9872541195298521e93d9c5f9140933a0a0613
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-10-26 14:28:47 +0700

    Hero Section Light Mode : added

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
 
 			{/* Hero Headline */}
 			<div className="relative m-auto mt-9">
-				<h1 className="text-5xl font-semibold text-center text-white">
+				<h1 className="text-5xl font-semibold text-center text-black dark:text-white">
 					Crafting pixels to make{" "}
 					<span className="text-transparent bg-gradient-to-r from-primary via-purple-500 to-pink-500 bg-clip-text">
 						dreams
@@ -23,7 +23,7 @@ const Hero = () => {
 
 			{/* Hero Description */}
 			<div className="relative m-auto mt-8">
-				<p className="text-lg font-light leading-8 text-center text-white">
+				<p className="text-lg font-light leading-8 text-center text-black dark:text-white">
 					A <span className="text-primary">tech-savvy artist</span>{" "}
 					crafting interfaces for over 4 years, blending the art of
 					Product Design with Front-end Development.
@@ -32,7 +32,7 @@ const Hero = () => {
 
 			{/* Main Button */}
 			<div className="flex justify-center mt-14">
-				<button className="pt-3 pb-[15px] text-[17px] font-medium text-black bg-white rounded-xl w-fit h-fit px-10">
+				<button className="pt-3 pb-[15px] text-[17px] font-medium dark:text-black text-white dark:bg-white bg-black rounded-xl w-fit h-fit px-10">
 					Let's talk!
 				</button>
 			</div>
