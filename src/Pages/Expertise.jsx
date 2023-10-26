b217c834cba653e07f725d88051a7cc6556e78a3
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-10-26 15:36:19 +0700

    Expertise Section Light Mode : added

--- src/Pages/Expertise.jsx
+++ src/Pages/Expertise.jsx
@@ -11,7 +11,7 @@ const Expertise = () => {
 			className="relative w-full h-auto pt-20 pb-40 px-sectionpl"
 		>
 			{/* Expertise Title */}
-			<h1 className="text-5xl font-semibold text-center text-white">
+			<h1 className="text-5xl font-semibold text-center text-black dark:text-white">
 				Delivering results that go{" "}
 				<span className="text-transparent bg-gradient-to-r from-primary via-purple-500 to-pink-500 bg-clip-text">
 					beyond expectations
@@ -21,8 +21,8 @@ const Expertise = () => {
 			{/* Expertise Container 1 */}
 			<div className="flex flex-col items-center justify-between pt-20 gap-7 h-fit">
 				{/* UI/UX Design */}
-				<div className="w-full px-6 py-5 text-white bg-accent rounded-xl">
-					<div className="flex items-center justify-center px-[14px] py-[14px] bg-[#21254c] rounded-md w-fit h-fit">
+				<div className="w-full px-6 py-5 text-black bg-white dark:text-white dark:bg-accent rounded-xl">
+					<div className="flex items-center justify-center px-[14px] py-[14px] dark:bg-[#21254c] bg-lightgrey rounded-md w-fit h-fit">
 						<img
 							src={uiuxdesign}
 							alt="uiuxdesign"
@@ -32,21 +32,21 @@ const Expertise = () => {
 					<h1 className="text-[23px] mt-6 font-semibold">
 						UI/UX Design
 					</h1>
-					<p className="mt-3 text-lg font-light">
+					<p className="mt-3 text-lg font-normal dark:font-light">
 						I am highly-skilled at architecting User Interface and
 						User Experience for mobile apps and websites.
 					</p>
 				</div>
 
 				{/* Project Planning */}
-				<div className="w-full px-6 py-5 text-white bg-accent rounded-xl">
-					<div className="flex items-center justify-center px-[14px] py-[14px] bg-[#21254c] rounded-md w-fit h-fit">
+				<div className="w-full px-6 py-5 text-black bg-white dark:text-white dark:bg-accent rounded-xl">
+					<div className="flex items-center justify-center px-[14px] py-[14px] dark:bg-[#21254c] bg-lightgrey rounded-md w-fit h-fit">
 						<img src={planning} alt="planning" className="w-6" />
 					</div>
 					<h1 className="text-[23px] mt-6 font-semibold">
 						Project Planning
 					</h1>
-					<p className="mt-3 text-lg font-light">
+					<p className="mt-3 text-lg font-normal dark:font-light">
 						I tend to create wireframe, user-flow, and user-persona
 						to determine intended features and functionality.
 					</p>
@@ -56,8 +56,8 @@ const Expertise = () => {
 			{/* Expertise Container 2 */}
 			<div className="flex flex-col items-center justify-between pt-7 gap-7 h-fit">
 				{/* Prototyping */}
-				<div className="w-full px-6 py-5 text-white bg-accent rounded-xl">
-					<div className="flex items-center justify-center px-[14px] py-[14px] bg-[#21254c] rounded-md w-fit h-fit">
+				<div className="w-full px-6 py-5 text-black bg-white dark:text-white dark:bg-accent rounded-xl">
+					<div className="flex items-center justify-center px-[14px] py-[14px] dark:bg-[#21254c] bg-lightgrey rounded-md w-fit h-fit">
 						<img
 							src={prototyping}
 							alt="prototyping"
@@ -67,21 +67,21 @@ const Expertise = () => {
 					<h1 className="text-[23px] mt-6 font-semibold">
 						Prototyping
 					</h1>
-					<p className="mt-3 text-lg font-light">
+					<p className="mt-3 text-lg font-normal dark:font-light">
 						An interactive and clickable prototype is needed to
 						carry out the usability testing and evaluation.
 					</p>
 				</div>
 
 				{/* Front-end Development */}
-				<div className="w-full px-6 py-5 text-white bg-accent rounded-xl">
-					<div className="flex items-center justify-center px-[14px] py-[14px] bg-[#21254c] rounded-md w-fit h-fit">
+				<div className="w-full px-6 py-5 text-black bg-white dark:text-white dark:bg-accent rounded-xl">
+					<div className="flex items-center justify-center px-[14px] py-[14px] dark:bg-[#21254c] bg-lightgrey rounded-md w-fit h-fit">
 						<img src={frontend} alt="frontend" className="w-6" />
 					</div>
 					<h1 className="text-[23px] mt-6 font-semibold">
 						Front-end Development
 					</h1>
-					<p className="mt-3 text-lg font-light">
+					<p className="mt-3 text-lg font-normal dark:font-light">
 						I am skilled in building dynamic web-app using React,
 						HTML, CSS and any other relevant Frameworks.
 					</p>
