cfe226113bccb23e5ce267b2ee062315fafce712
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-10-27 10:42:37 +0700

    About Section Padding X : fixed

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -8,66 +8,75 @@ import behance from "../Assets/behance.png";
 
 const About = () => {
 	return (
-		<div
-			id="about"
-			className="relative w-full h-auto pt-20 pb-40 px-sectionpl"
-		>
-			{/* About Title */}
-			<h1 className="text-5xl font-semibold text-center text-black dark:text-white">
-				Delivering results that go{" "}
-				<span className="text-transparent bg-gradient-to-r from-primary via-purple-500 to-pink-500 bg-clip-text">
-					beyond expectations
-				</span>{" "}
-			</h1>
+		<>
+			<div
+				id="about"
+				className="relative w-full h-auto pt-20 px-sectionpl"
+			>
+				{/* About Title */}
+				<h1 className="text-5xl font-semibold text-center text-black dark:text-white">
+					Delivering results that go{" "}
+					<span className="text-transparent bg-gradient-to-r from-primary via-purple-500 to-pink-500 bg-clip-text">
+						beyond expectations
+					</span>{" "}
+				</h1>
+			</div>
 
 			{/* About Decoration */}
-			<div className="relative flex mt-20 bg-purple-600 pt-14 h-fit rounded-3xl">
-				<img
-					src={picc}
-					alt="picc"
-					className="relative object-contain w-10/12 h-full origin-bottom-left"
-				/>
-				<div className="absolute bottom-0 w-full px-6 py-6 bg-none h-fit">
-					<div className="px-4 py-3 text-black rounded-xl backdrop-blur-lg bg-slate-50 bg-opacity-70">
-						<div className="flex items-center justify-between">
-							<div className="flex items-center justify-between gap-3">
-								<img
-									src={imessage}
-									alt="imessage"
-									className="w-[18px]"
-								/>
-								<h1 className="text-sm font-medium text-gray-600">
-									MESSAGES
+			<div className="mt-20 px-sectionpl">
+				<div className="relative flex bg-purple-600 pt-14 h-fit rounded-3xl">
+					<img
+						src={picc}
+						alt="picc"
+						className="relative object-contain w-10/12 h-full origin-bottom-left"
+					/>
+					<div className="absolute bottom-0 w-full px-6 py-6 bg-none h-fit">
+						<div className="px-4 py-3 text-black rounded-xl backdrop-blur-lg bg-slate-50 bg-opacity-70">
+							<div className="flex items-center justify-between">
+								<div className="flex items-center justify-between gap-3">
+									<img
+										src={imessage}
+										alt="imessage"
+										className="w-[18px]"
+									/>
+									<h1 className="text-sm font-medium text-gray-600">
+										MESSAGES
+									</h1>
+								</div>
+								<h1 className="text-sm font-medium text-gray-500">
+									now
 								</h1>
 							</div>
-							<h1 className="text-sm font-medium text-gray-500">
-								now
-							</h1>
+							<p className="mt-2 font-medium text-md">
+								Alif Firdaus
+							</p>
+							<p className="font-normal text-md">
+								Hi there! get to know about me 👋
+							</p>
 						</div>
-						<p className="mt-2 font-medium text-md">Alif Firdaus</p>
-						<p className="font-normal text-md">
-							Hi there! get to know about me 👋
-						</p>
 					</div>
 				</div>
 			</div>
 
 			{/* About Line */}
-			<div className="flex justify-center mt-20">
+			<div className="flex justify-center mt-20 px-sectionpl">
 				<div className="w-20 h-1 bg-black rounded-full dark:bg-white"></div>
 			</div>
 
 			{/* About Description */}
-			<p className="z-40 mt-16 text-lg font-normal leading-8 text-center text-black dark:font-light dark:text-white">
-				I am Alif Firdaus, a skilled Product Designer and Front-end
-				Developer renowned for exceeding expectations with my meticulous
-				attention to detail and in-depth understanding of the harmony
-				between Product Interface and User Experience. With my
-				expertise, I am confident to bring your ideas to life!
-			</p>
+			<div className="px-sectionpl">
+				<p className="z-40 mt-16 text-lg font-normal leading-8 text-center text-black dark:font-light dark:text-white">
+					I am Alif Firdaus, a skilled Product Designer and Front-end
+					Developer renowned for exceeding expectations with my
+					meticulous attention to detail and in-depth understanding of
+					the harmony between Product Interface and User Experience.
+					With my expertise, I am confident to bring your ideas to
+					life!
+				</p>
+			</div>
 
 			{/* Socials Container 2 */}
-			<div className="flex flex-col items-center justify-between pt-20 gap-7 h-fit">
+			<div className="flex flex-col items-center justify-between pt-20 gap-7 h-fit px-sectionpl">
 				{/* Linkedin */}
 				<div className="w-full px-6 py-5 text-black bg-white dark:text-white dark:bg-accent rounded-xl">
 					<div className="flex items-center justify-center px-[14px] py-[14px] bg-green-700 rounded-md w-fit h-fit">
@@ -110,7 +119,7 @@ const About = () => {
 			</div>
 
 			{/* Socials Container 2 */}
-			<div className="flex flex-col items-center justify-between pt-7 gap-7 h-fit">
+			<div className="flex flex-col items-center justify-between pb-40 pt-7 gap-7 h-fit px-sectionpl">
 				{/* Github */}
 				<div className="w-full px-6 py-5 text-black bg-white dark:text-white dark:bg-accent rounded-xl">
 					<div className="flex items-center justify-center px-[14px] py-[14px] bg-purple-700 rounded-md w-fit h-fit">
@@ -151,7 +160,7 @@ const About = () => {
 					</a>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
