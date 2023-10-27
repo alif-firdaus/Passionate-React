d2ea50e78bd929e5522e7ef3e9a548445d53dfc3
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-10-27 14:56:38 +0700

    Getintouch Section md : added

--- src/Pages/Getintouch.jsx
+++ src/Pages/Getintouch.jsx
@@ -7,7 +7,7 @@ const Getintouch = () => {
 			className="pt-20 pb-56 h-fit dark:bg-base bg-lightgrey px-sectionpl"
 		>
 			{/* Get in Touch Title */}
-			<h1 className="text-5xl font-semibold text-center text-black dark:text-white">
+			<h1 className="text-5xl font-semibold text-center text-black md:text-6xl dark:text-white">
 				Turning your ideas into{" "}
 				<span className="text-transparent bg-gradient-to-r from-primary via-purple-500 to-pink-500 bg-clip-text">
 					real-life
@@ -16,7 +16,7 @@ const Getintouch = () => {
 			</h1>
 
 			{/* Main Button */}
-			<div className="flex justify-center mt-14">
+			<div className="flex justify-center mt-14 md:mt-16">
 				<button className="pt-3 pb-[15px] text-[17px] font-medium dark:text-black text-white dark:bg-white bg-black rounded-xl w-fit h-fit px-10">
 					Let's talk!
 				</button>
