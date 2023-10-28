a522b1f0d2be19f8ce559ce7df3a0f63e2724d44
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-10-28 15:17:48 +0700

    Navbar : fixed

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -52,23 +52,23 @@ function Navbar() {
 	return (
 		<nav className="sticky top-0 z-10 flex items-center justify-between py-4 border-b-[1px] backdrop-blur-lg dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20">
 			{/* Navbar Title */}
-			<div className="flex items-center font-semibold cursor-pointer text-[24px] pb-[2px] dark:text-white text-black ml-[26px]">
+			<div className="flex items-center font-semibold cursor-pointer text-[24px] pb-[2px] dark:text-white text-black ml-[26px] lg:ml-32">
 				Passionate
 			</div>
 
 			{/* Navbar Links */}
 			<div
-				className={`fixed gap-4 pt-6 w-full flex flex-col px-sectionpl h-screen dark:bg-base bg-white z-[1] ${
-					isOpen ? "top-[73px]" : "-left-full"
+				className={`fixed lg:flex lg:relative lg:flex-row gap-4 lg:gap-10 pt-6 lg:py-0 w-full lg:items-center lg:justify-between lg:w-fit flex flex-col px-sectionpl h-screen lg:h-fit dark:bg-base bg-white z-[1] ${
+					isOpen ? "top-[73px] lg:top-0" : "-left-full"
 				}`}
 			>
 				{menuOptions.map((option, index) => (
 					<div
-						className="w-full transition-all duration-500 ease-in-out"
+						className="w-full transition-all duration-500 ease-in-out lg:flex lg:items-center lg:w-fit"
 						key={index}
 					>
 						<a
-							className="flex w-full pb-5 items-center text-lg font-light dark:text-white text-black dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20 border-b-[1px]"
+							className="flex w-full lg:w-fit pb-5 lg:py-0 items-center text-lg lg:text-base font-light dark:text-white text-black dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20 border-b-[1px] lg:border-none"
 							href={option.href}
 							onClick={() => {
 								setIsOpen(!isOpen);
@@ -81,9 +81,9 @@ function Navbar() {
 			</div>
 
 			{/* Navbar Extra */}
-			<div className="flex justify-between gap-4 mr-[26px]">
+			<div className="flex justify-between gap-4 lg:gap-5 mr-[26px] lg:mr-32">
 				<div
-					className="flex justify-center border-[1px] w-[40px] h-[40px] rounded-lg dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20 items-center cursor-pointer"
+					className="flex justify-center border-[1px] w-[40px] h-[40px] rounded-lg dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20 lg:bg-accent lg:border-none items-center cursor-pointer"
 					onClick={handleThemeSwitch}
 				>
 					{theme === "dark" ? (
@@ -97,13 +97,19 @@ function Navbar() {
 					onClick={() => {
 						setIsOpen(!isOpen);
 					}}
-					className="flex justify-center border-[1px] w-[40px] h-[40px] rounded-lg dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20 items-center cursor-pointer"
+					className="flex lg:hidden justify-center border-[1px] w-[40px] h-[40px] rounded-lg dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20 items-center cursor-pointer"
 				>
 					{isOpen ? (
 						<XMarkIcon className="w-6 stroke-[1.3px] dark:text-white text-black" />
 					) : (
 						<Bars3Icon className="w-6 stroke-[1.3px] dark:text-white text-black" />
 					)}
+				</div>
+
+				<div className="lg:flex lg:items-center lg:justify-center sm:hidden md:hidden">
+					<button className="text-black text-[15px] font-medium bg-white h-[40px] px-6 pb-[2px] rounded-lg">
+						Connect
+					</button>
 				</div>
 			</div>
 		</nav>
