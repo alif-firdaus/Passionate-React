85014425c74459888447c2747b6f21bbbde884b3
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-10-28 19:16:43 +0700

    Hero Section lg : fixed

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Hero = () => {
 	return (
-		<div id="hero" className="pt-20 pb-20 lg:pt-28 h-fit px-sectionpl">
+		<div
+			id="hero"
+			className="pt-20 pb-20 lg:pb-36 lg:pt-24 h-fit px-sectionpl"
+		>
 			{/* Hero Identity */}
 			<div className="relative px-6 py-2 m-auto align-middle rounded-full dark:bg-accent bg-lightblue w-fit h-fit">
 				<p className="text-center text-[16px] dark:font-medium font-semibold text-primary">
@@ -11,8 +14,8 @@ const Hero = () => {
 			</div>
 
 			{/* Hero Headline */}
-			<div className="relative m-auto mt-9 lg:mt-14 lg:px-52">
-				<h1 className="text-5xl font-semibold text-center text-black lg:text-[90px] md:text-6xl dark:text-white">
+			<div className="relative m-auto mt-9 lg:mt-12 lg:px-52 px">
+				<h1 className="text-5xl font-semibold text-center text-black lg:text-[86px] md:text-6xl dark:text-white">
 					Crafting pixels to make{" "}
 					<span className="text-transparent bg-gradient-to-r from-primary via-purple-500 to-pink-500 bg-clip-text">
 						dreams
@@ -22,7 +25,7 @@ const Hero = () => {
 			</div>
 
 			{/* Hero Description */}
-			<div className="relative m-auto mt-8 lg:mt-14 lg:px-96">
+			<div className="relative m-auto mt-8 lg:mt-11 lg:px-[300px]">
 				<p className="text-lg font-normal leading-8 text-center text-black lg:text-[20px] lg:leading-10 dark:font-light dark:text-white">
 					A <span className="text-primary">tech-savvy artist</span>{" "}
 					crafting interfaces for over 4 years, blending the art of
@@ -31,7 +34,7 @@ const Hero = () => {
 			</div>
 
 			{/* Main Button */}
-			<div className="flex justify-center mt-14">
+			<div className="flex justify-center mt-14 lg:mt-16">
 				<button className="pt-3 pb-[15px] text-[17px] font-medium dark:text-black text-white dark:bg-white bg-black rounded-xl w-fit h-fit px-10">
 					Let's talk!
 				</button>
