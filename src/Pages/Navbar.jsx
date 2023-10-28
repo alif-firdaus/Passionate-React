d1d703c576118a664b59d7f904b8184e2154dee7
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-10-28 19:52:05 +0700

    Navbar lg : fixed

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -52,64 +52,68 @@ function Navbar() {
 	return (
 		<nav className="sticky top-0 z-10 flex items-center justify-between py-4 border-b-[1px] backdrop-blur-lg dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20">
 			{/* Navbar Title */}
-			<div className="flex items-center font-semibold cursor-pointer text-[24px] pb-[2px] dark:text-white text-black ml-[26px] lg:ml-32">
+			<div className="flex items-center font-semibold cursor-pointer text-[24px] pb-[2px] dark:text-white text-black ml-[26px] lg:ml-20">
 				Passionate
 			</div>
 
-			{/* Navbar Links */}
-			<div
-				className={`fixed lg:flex lg:relative lg:flex-row gap-4 lg:gap-10 pt-6 lg:py-0 w-full lg:items-center lg:justify-between lg:w-fit flex flex-col px-sectionpl h-screen lg:h-fit dark:bg-base md:dark:bg-base bg-white md:bg-white lg:bg-transparent lg:dark:bg-transparent z-[1] ${
-					isOpen ? "top-[73px] lg:top-0" : "-left-full lg:left-0"
-				}`}
-			>
-				{menuOptions.map((option, index) => (
-					<div
-						className="w-full transition-all duration-500 ease-in-out"
-						key={index}
-					>
-						<a
-							className="flex w-full lg:w-fit pb-5 lg:py-0 items-center text-lg lg:text-base font-light dark:text-white text-black dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20 border-b-[1px] lg:border-none"
-							href={option.href}
-							onClick={() => {
-								setIsOpen(!isOpen);
-							}}
+			<div className="flex items-center justify-between">
+				{/* Navbar Links */}
+				<div
+					className={`fixed lg:flex lg:relative lg:flex-row gap-4 lg:gap-10 pt-6 lg:py-0 w-full lg:items-center lg:justify-between lg:w-fit flex flex-col px-sectionpl lg:px-0 h-screen lg:h-fit dark:bg-base md:dark:bg-base bg-white md:bg-white lg:bg-transparent lg:dark:bg-transparent z-[1] ${
+						isOpen ? "top-[73px] lg:top-0" : "-left-full lg:left-0"
+					}`}
+				>
+					{menuOptions.map((option, index) => (
+						<div
+							className="w-full transition-all duration-500 ease-in-out"
+							key={index}
 						>
-							{option.text}
-						</a>
+							<a
+								className="flex w-full lg:w-fit pb-5 lg:py-0 items-center text-lg lg:text-[15px] font-light lg:font-normal dark:text-white text-black dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20 border-b-[1px] lg:border-none lg:hover:text-grey duration-300"
+								href={option.href}
+								onClick={() => {
+									setIsOpen(!isOpen);
+								}}
+							>
+								{option.text}
+							</a>
+						</div>
+					))}
+				</div>
+
+				<div className="hidden lg:flex w-[1px] h-7 bg-slate-50 bg-opacity-20 mx-11"></div>
+
+				{/* Navbar Extra */}
+				<div className="flex justify-between gap-4 lg:gap-6 mr-[26px] lg:mr-20">
+					<div
+						className="flex justify-center border-[1px] w-[40px] h-[40px] rounded-lg dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20 lg:bg-accent lg:border-none items-center cursor-pointer"
+						onClick={handleThemeSwitch}
+					>
+						{theme === "dark" ? (
+							<MoonIcon className="w-[20px] text-primary" />
+						) : (
+							<SunIcon className="w-[24px] text-black " />
+						)}
 					</div>
-				))}
-			</div>
 
-			{/* Navbar Extra */}
-			<div className="flex justify-between gap-4 lg:gap-5 mr-[26px] lg:mr-32">
-				<div
-					className="flex justify-center border-[1px] w-[40px] h-[40px] rounded-lg dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20 lg:bg-accent lg:border-none items-center cursor-pointer"
-					onClick={handleThemeSwitch}
-				>
-					{theme === "dark" ? (
-						<MoonIcon className="w-[20px] text-primary" />
-					) : (
-						<SunIcon className="w-[24px] text-black " />
-					)}
-				</div>
+					<div
+						onClick={() => {
+							setIsOpen(!isOpen);
+						}}
+						className="flex lg:hidden justify-center border-[1px] w-[40px] h-[40px] rounded-lg dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20 items-center cursor-pointer"
+					>
+						{isOpen ? (
+							<XMarkIcon className="w-6 stroke-[1.3px] dark:text-white text-black" />
+						) : (
+							<Bars3Icon className="w-6 stroke-[1.3px] dark:text-white text-black" />
+						)}
+					</div>
 
-				<div
-					onClick={() => {
-						setIsOpen(!isOpen);
-					}}
-					className="flex lg:hidden justify-center border-[1px] w-[40px] h-[40px] rounded-lg dark:border-slate-50 border-slate-400 dark:border-opacity-10 border-opacity-20 items-center cursor-pointer"
-				>
-					{isOpen ? (
-						<XMarkIcon className="w-6 stroke-[1.3px] dark:text-white text-black" />
-					) : (
-						<Bars3Icon className="w-6 stroke-[1.3px] dark:text-white text-black" />
-					)}
-				</div>
-
-				<div className="hidden lg:flex lg:items-center lg:justify-center ">
-					<button className="text-black text-[15px] font-medium bg-white h-[40px] px-6 pb-[2px] rounded-lg hover:bg-grey duration-300">
-						Connect
-					</button>
+					<div className="items-center justify-center hidden lg:flex">
+						<button className="text-black text-[15px] font-medium bg-white h-[40px] px-6 pb-[2px] rounded-lg hover:bg-grey duration-300">
+							Connect
+						</button>
+					</div>
 				</div>
 			</div>
 		</nav>
