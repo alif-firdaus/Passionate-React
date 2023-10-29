c2f57c8c257e7e6993a59f48474864129c6a0571
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-10-29 21:40:53 +0700

    Getintouch Section : fixed

--- src/Pages/Getintouch.jsx
+++ src/Pages/Getintouch.jsx
@@ -9,15 +9,15 @@ const Getintouch = () => {
 			{/* Get in Touch Title */}
 			<h1 className="text-5xl font-semibold text-center text-black md:text-6xl dark:text-white">
 				Turning your ideas into{" "}
-				<span className="text-transparent bg-gradient-to-r from-primary via-purple-500 to-pink-500 bg-clip-text">
+				<span className="text-transparent bg-gradient-to-r dark:from-darkprimary from-lightprimary dark:via-purple-500 dark:to-pink-500 via-purple-500 to-pink-500 bg-clip-text">
 					real-life
 				</span>{" "}
 				products is my calling
 			</h1>
 
 			{/* Main Button */}
-			<div className="flex justify-center mt-14 md:mt-16">
-				<button className="pt-3 pb-[15px] text-[17px] font-medium dark:text-black text-white dark:bg-white bg-black rounded-xl w-fit h-fit px-10">
+			<div className="flex justify-center mt-16 md:mt-16">
+				<button className="pt-3 pb-[15px] text-[17px] font-medium dark:text-black text-white dark:bg-white bg-black rounded-xl w-fit h-fit px-10 dark:hover:bg-softgrey hover:bg-grey duration-300">
 					Let's talk!
 				</button>
 			</div>
