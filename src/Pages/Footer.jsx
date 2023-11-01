f98398bb59295688c61e64f3fe28e15041cb27c2
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-01 20:06:17 +0700

    Footer Light Mode : updated

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -10,23 +10,23 @@ const Footer = () => {
 	return (
 		<div
 			id="footer"
-			className="relative w-full h-auto pb-12 pt-14 bg-gradient-to-br from-purple-800 to-purple-950"
+			className="relative w-full h-auto pb-12 pt-14 border-t-[1px] dark:border-slate-50 border-slate-400 dark:border-opacity-20 border-opacity-40"
 		>
 			{/* Footer Title */}
 			<div className="px-sectionpxsm">
-				<div className="flex items-center font-semibold cursor-pointer text-[24px] pb-[2px] text-white">
+				<div className="flex items-center font-semibold cursor-pointer text-[24px] pb-[2px] dark:text-white text-black">
 					Passionate
 				</div>
 
 				{/* Footer Description */}
-				<p className="mt-2 text-white">
+				<p className="mt-2 text-black dark:text-white">
 					Crafting pixels to make dreams come true
 				</p>
 
 				{/* Footer Socials */}
 				<div className="flex gap-5 mt-7">
 					{/* Linkedin */}
-					<div className="items-center justify-center p-[14px] bg-purple-700 rounded-lg w-fit h-fit bg-opacity-60">
+					<div className="items-center justify-center p-[14px] dark:bg-darktertiary bg-purple-700 rounded-lg w-fit h-fit dark:bg-opacity-60">
 						<a
 							href="https://www.linkedin.com/in/aliffirdaus97/"
 							target="_blank"
@@ -41,7 +41,7 @@ const Footer = () => {
 					</div>
 
 					{/* Dribbble */}
-					<div className="items-center justify-center p-[14px] bg-purple-700 rounded-lg w-fit h-fit bg-opacity-60">
+					<div className="items-center justify-center p-[14px] dark:bg-darktertiary bg-orange-600 rounded-lg w-fit h-fit dark:bg-opacity-60">
 						<a
 							href="https://dribbble.com/aliffirdaus"
 							target="_blank"
@@ -56,7 +56,7 @@ const Footer = () => {
 					</div>
 
 					{/* Github */}
-					<div className="items-center justify-center p-[14px] bg-purple-700 rounded-lg w-fit h-fit bg-opacity-60">
+					<div className="items-center justify-center p-[14px] dark:bg-darktertiary bg-blue-700 rounded-lg w-fit h-fit dark:bg-opacity-60">
 						<a
 							href="https://github.com/alif-firdaus"
 							target="_blank"
@@ -67,7 +67,7 @@ const Footer = () => {
 					</div>
 
 					{/* Behance */}
-					<div className="items-center justify-center p-[14px] bg-purple-700 rounded-lg w-fit h-fit bg-opacity-60">
+					<div className="items-center justify-center p-[14px] dark:bg-darktertiary bg-green-700 rounded-lg w-fit h-fit dark:bg-opacity-60">
 						<a
 							href="https://www.behance.net/aliffirdaus97"
 							target="_blank"
@@ -79,31 +79,43 @@ const Footer = () => {
 				</div>
 
 				{/* Navigation */}
-				<div className="flex items-center mt-16 font-semibold cursor-pointer text-[22px] pb-[2px] text-white">
+				<div className="flex items-center mt-16 font-semibold cursor-pointer text-[22px] pb-[2px] dark:text-white text-black">
 					Navigation
 				</div>
 
 				{/* Navigation Links */}
 				<div className="flex flex-col gap-4 mt-5">
-					<p className="text-[18px] text-white">Home</p>
-					<p className="text-[18px] text-white">About</p>
-					<p className="text-[18px] text-white">Expertise</p>
-					<p className="text-[18px] text-white">Experience</p>
-					<p className="text-[18px] text-white">Projects</p>
+					<p className="text-[18px] dark:text-white text-black">
+						Home
+					</p>
+					<p className="text-[18px] dark:text-white text-black">
+						About
+					</p>
+					<p className="text-[18px] dark:text-white text-black">
+						Expertise
+					</p>
+					<p className="text-[18px] dark:text-white text-black">
+						Experience
+					</p>
+					<p className="text-[18px] dark:text-white text-black">
+						Projects
+					</p>
 				</div>
 
 				{/* Connect */}
-				<div className="flex items-center mt-16 font-semibold cursor-pointer text-[22px] pb-[2px] text-white">
+				<div className="flex items-center mt-16 font-semibold cursor-pointer text-[22px] pb-[2px] dark:text-white text-black">
 					Connect
 				</div>
 
 				{/* Connect List */}
 				<div className="flex flex-col gap-4 mt-5">
-					<p className="text-[18px] text-white">+62 813-3666-4586</p>
-					<p className="text-[18px] text-white">
+					<p className="text-[18px] dark:text-white text-black">
+						+62 813-3666-4586
+					</p>
+					<p className="text-[18px] dark:text-white text-black">
 						www.aliffirdaus.com
 					</p>
-					<p className="text-[18px] text-white">
+					<p className="text-[18px] dark:text-white text-black">
 						aliffirdaus@petaniweb.com
 					</p>
 				</div>
@@ -111,7 +123,7 @@ const Footer = () => {
 
 			{/* Footer Bottom */}
 			<div className="w-full pt-10 mt-16 border-t-[1px] border-opacity-30 border-slate-50 px-sectionpxsm">
-				<p className="text-[18px] text-white">
+				<p className="text-[18px] dark:text-white text-black">
 					&#169; Passionate Personal Website
 				</p>
 			</div>
