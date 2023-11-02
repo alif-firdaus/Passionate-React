ca6fce4b1d7b2e1c45e445b51a6ad616a0bb699e
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-02 19:58:09 +0700

    Footer Section lg : added

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
 		<>
 			<div
 				id="footer"
-				className="relative w-full h-auto pb-12 pt-14 border-t-[1px] dark:border-slate-50 border-slate-400 dark:border-opacity-20 border-opacity-40 backdrop-blur-lg"
+				className="relative w-full h-auto pb-12 pt-14 lg:pt-20 border-t-[1px] dark:border-slate-50 border-slate-400 dark:border-opacity-20 border-opacity-40 backdrop-blur-lg"
 			>
 				<img
 					src={pattern4}
@@ -28,124 +28,130 @@ const Footer = () => {
 					className="absolute bottom-0 hidden w-screen origin-bottom-right opacity-20 lg:block"
 				/>
 				{/* Footer Title */}
-				<div className="px-sectionpxsm">
-					<div className="flex items-center font-semibold cursor-pointer text-[24px] pb-[2px] dark:text-white text-black">
-						Passionate
-					</div>
-
-					{/* Footer Description */}
-					<p className="mt-2 text-black dark:text-white">
-						Crafting pixels to make dreams come true
-					</p>
-
-					{/* Footer Socials */}
-					<div className="flex gap-5 mt-7">
-						{/* Linkedin */}
-						<div className="items-center justify-center p-[14px] dark:bg-darktertiary bg-purple-700 rounded-lg w-fit h-fit dark:bg-opacity-60">
-							<a
-								href="https://www.linkedin.com/in/aliffirdaus97/"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<img
-									src={linkedin}
-									alt="linkedin"
-									className="w-5"
-								/>
-							</a>
+				<div className="flex flex-col justify-between px-sectionpxsm lg:px-sectionpxlg lg:flex-row">
+					<div className="flex flex-col lg:z-10">
+						<div className="flex items-center font-semibold cursor-pointer text-[24px] pb-[2px] dark:text-white text-black">
+							Passionate
 						</div>
 
-						{/* Dribbble */}
-						<div className="items-center justify-center p-[14px] dark:bg-darktertiary bg-orange-600 rounded-lg w-fit h-fit dark:bg-opacity-60">
-							<a
-								href="https://dribbble.com/aliffirdaus"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<img
-									src={dribbble}
-									alt="dribbble"
-									className="w-5"
-								/>
-							</a>
-						</div>
+						{/* Footer Description */}
+						<p className="mt-2 text-black dark:text-white">
+							Crafting pixels to make dreams come true
+						</p>
 
-						{/* Github */}
-						<div className="items-center justify-center p-[14px] dark:bg-darktertiary bg-blue-700 rounded-lg w-fit h-fit dark:bg-opacity-60">
-							<a
-								href="https://github.com/alif-firdaus"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<img
-									src={github}
-									alt="github"
-									className="w-5"
-								/>
-							</a>
-						</div>
+						{/* Footer Socials */}
+						<div className="flex gap-5 mt-7">
+							{/* Linkedin */}
+							<div className="items-center justify-center p-[14px] dark:bg-darktertiary bg-purple-700 rounded-lg w-fit h-fit dark:bg-opacity-60 cursor-pointer">
+								<a
+									href="https://www.linkedin.com/in/aliffirdaus97/"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src={linkedin}
+										alt="linkedin"
+										className="w-5"
+									/>
+								</a>
+							</div>
 
-						{/* Behance */}
-						<div className="items-center justify-center p-[14px] dark:bg-darktertiary bg-green-700 rounded-lg w-fit h-fit dark:bg-opacity-60">
-							<a
-								href="https://www.behance.net/aliffirdaus97"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<img
-									src={behance}
-									alt="behance"
-									className="w-5"
-								/>
-							</a>
+							{/* Dribbble */}
+							<div className="items-center justify-center p-[14px] dark:bg-darktertiary bg-orange-600 rounded-lg w-fit h-fit dark:bg-opacity-60 cursor-pointer">
+								<a
+									href="https://dribbble.com/aliffirdaus"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src={dribbble}
+										alt="dribbble"
+										className="w-5"
+									/>
+								</a>
+							</div>
+
+							{/* Github */}
+							<div className="items-center justify-center p-[14px] dark:bg-darktertiary bg-blue-700 rounded-lg w-fit h-fit dark:bg-opacity-60 cursor-pointer">
+								<a
+									href="https://github.com/alif-firdaus"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src={github}
+										alt="github"
+										className="w-5"
+									/>
+								</a>
+							</div>
+
+							{/* Behance */}
+							<div className="items-center justify-center p-[14px] dark:bg-darktertiary bg-green-700 rounded-lg w-fit h-fit dark:bg-opacity-60 cursor-pointer">
+								<a
+									href="https://www.behance.net/aliffirdaus97"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<img
+										src={behance}
+										alt="behance"
+										className="w-5"
+									/>
+								</a>
+							</div>
 						</div>
 					</div>
 
 					{/* Navigation */}
-					<div className="flex items-center mt-16 font-semibold cursor-pointer text-[22px] pb-[2px] dark:text-white text-black">
-						Navigation
-					</div>
+					<div className="flex flex-col mt-16 lg:mt-0">
+						<div className="flex items-center font-semibold cursor-pointer text-[22px] pb-[2px] dark:text-white text-black">
+							Navigation
+						</div>
 
-					{/* Navigation Links */}
-					<div className="flex flex-col gap-4 mt-5">
-						<p className="text-[18px] dark:text-white text-black">
-							<a href="#home">Home</a>
-						</p>
-						<p className="text-[18px] dark:text-white text-black">
-							<a href="#about">About</a>
-						</p>
-						<p className="text-[18px] dark:text-white text-black">
-							<a href="#expertise">Expertise</a>
-						</p>
-						<p className="text-[18px] dark:text-white text-black">
-							<a href="#experience">Experience</a>
-						</p>
-						<p className="text-[18px] dark:text-white text-black">
-							<a href="#projects">Projects</a>
-						</p>
+						{/* Navigation Links */}
+						<div className="flex flex-col gap-4 mt-5 lg:z-10">
+							<p className="text-[18px] dark:text-white text-black cursor-pointer">
+								<a href="#home">Home</a>
+							</p>
+							<p className="text-[18px] dark:text-white text-black cursor-pointer">
+								<a href="#about">About</a>
+							</p>
+							<p className="text-[18px] dark:text-white text-black cursor-pointer">
+								<a href="#expertise">Expertise</a>
+							</p>
+							<p className="text-[18px] dark:text-white text-black cursor-pointer">
+								<a href="#experience">Experience</a>
+							</p>
+							<p className="text-[18px] dark:text-white text-black cursor-pointer">
+								<a href="#projects">Projects</a>
+							</p>
+						</div>
 					</div>
 
 					{/* Connect */}
-					<div className="flex items-center mt-16 font-semibold cursor-pointer text-[22px] pb-[2px] dark:text-white text-black">
-						Connect
-					</div>
+					<div className="flex flex-col mt-16 lg:mt-0">
+						<div className="flex items-center font-semibold cursor-pointer text-[22px] pb-[2px] dark:text-white text-black">
+							Connect
+						</div>
 
-					{/* Connect List */}
-					<div className="flex flex-col gap-4 mt-5">
-						<p className="text-[18px] dark:text-white text-black">
-							+62 813-3666-4586
-						</p>
-						<p className="text-[18px] dark:text-white text-black">
-							www.aliffirdaus.com
-						</p>
-						<p className="text-[18px] dark:text-white text-black">
-							aliffirdaus@petaniweb.com
-						</p>
+						{/* Connect List */}
+						<div className="flex flex-col gap-4 mt-5">
+							<p className="text-[18px] dark:text-white text-black">
+								+62 813-3666-4586
+							</p>
+							<p className="text-[18px] dark:text-white text-black">
+								www.aliffirdaus.com
+							</p>
+							<p className="text-[18px] dark:text-white text-black">
+								aliffirdaus@petaniweb.com
+							</p>
+						</div>
 					</div>
 				</div>
 
 				{/* Footer Bottom */}
-				<div className="w-full pt-10 mt-16 border-t-[1px] border-opacity-30 border-slate-50 px-sectionpxsm">
+				<div className="w-full pt-10 mt-16 border-t-[1px] border-opacity-30 border-slate-50 px-sectionpxsm lg:px-sectionpxlg">
 					<p className="text-[18px] dark:text-white text-black">
 						&#169; Passionate Personal Website
 					</p>
