ec7971b6843a007fa41c3c3b7d8824f7d84598cc
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-02 15:21:51 +0700

    Projects Section : fixed

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -15,98 +15,56 @@ const Projects = () => {
 				so far
 			</h1>
 
-			{/* Projects Container 1 */}
-			<div className="flex flex-col items-center justify-between pt-20 gap-7 h-fit">
-				{/* Projects 1 */}
-				<div className="w-full px-6 py-5 text-black bg-white shadow-2xl dark:text-white dark:bg-darksecondary rounded-xl shadow-slate-200 dark:shadow-none">
-					<img
-						src={fixit}
-						alt="fixit"
-						className="relative object-cover rounded-t-lg"
-					/>
-					<h1 className="mt-6 text-[25px] font-semibold">
-						Fixit Mobile App
-					</h1>
-					<div className="w-10 h-[2px] mt-7 mb-9 bg-purple-400 rounded-full"></div>
-					<p className="mt-3 text-lg font-light">
-						This platform is where the users can find and recruit
-						professional service providers for their needs.
-					</p>
-					<a href="#" target="_blank" rel="noopener noreferrer">
-						<button className="px-5 pt-3 pb-[14px] dark:bg-darktertiary bg-lightbg font-medium mt-8 rounded-lg text-[16px] w-fit h-fit">
-							View Case Study
-						</button>
-					</a>
-				</div>
-
-				{/* Projects 2 */}
-				<div className="w-full px-6 py-5 text-black bg-white shadow-2xl dark:text-white dark:bg-darksecondary rounded-xl shadow-slate-200 dark:shadow-none">
-					<img
-						src={fixit}
-						alt="fixit"
-						className="relative object-cover rounded-t-lg"
-					/>
-					<h1 className="mt-6 text-[25px] font-semibold">
-						AQUA Crypto Wallet
-					</h1>
-					<div className="w-10 h-[2px] mt-7 mb-9 bg-purple-400 rounded-full"></div>
-					<p className="mt-3 text-lg font-light">
-						This platform is where the users can find and recruit
-						professional service providers for their needs.
-					</p>
-					<a href="#" target="_blank" rel="noopener noreferrer">
-						<button className="px-5 pt-3 pb-[14px] dark:bg-darktertiary bg-lightbg font-medium mt-8 rounded-lg text-[16px] w-fit h-fit">
-							View Case Study
-						</button>
-					</a>
-				</div>
+			{/* Projects 1 */}
+			<div className="w-full px-8 pb-10 mt-20 text-white bg-purple-700 pt-9 rounded-xl">
+				<p className="text-[15px]">FIXIT MOBILE APP</p>
+				<h1 className="text-5xl font-semibold mt-7">
+					Connecting people with local service provider
+				</h1>
+				<a href="#" target="_blank" rel="noopener noreferrer">
+					<button className="px-5 pt-3 pb-[14px] bg-purple-600 mt-14 rounded-lg text-[16px] w-fit h-fit">
+						Case Study coming soon
+					</button>
+				</a>
 			</div>
 
-			{/* Projects Container 2 */}
-			<div className="flex flex-col items-center justify-between pt-7 gap-7 h-fit">
-				{/* Projects 3 */}
-				<div className="w-full px-6 py-5 text-black bg-white shadow-2xl dark:text-white dark:bg-darksecondary rounded-xl shadow-slate-200 dark:shadow-none">
-					<img
-						src={fixit}
-						alt="fixit"
-						className="relative object-cover rounded-t-lg"
-					/>
-					<h1 className="mt-6 text-[25px] font-semibold">
-						Groceries Mobile App
-					</h1>
-					<div className="w-10 h-[2px] mt-7 mb-9 bg-purple-400 rounded-full"></div>
-					<p className="mt-3 text-lg font-light">
-						This platform is where the users can find and recruit
-						professional service providers for their needs.
-					</p>
-					<a href="#" target="_blank" rel="noopener noreferrer">
-						<button className="px-5 pt-3 pb-[14px] dark:bg-darktertiary bg-lightbg font-medium mt-8 rounded-lg text-[16px] w-fit h-fit">
-							View Case Study
-						</button>
-					</a>
-				</div>
+			{/* Projects 2 */}
+			<div className="w-full px-8 pb-10 text-white bg-orange-600 mt-7 pt-9 rounded-xl bg-opacity-90">
+				<p className="text-[15px]">AQUA WALLET</p>
+				<h1 className="text-5xl font-semibold mt-7">
+					Transferring and storing Crypto and NFTs
+				</h1>
+				<a href="#" target="_blank" rel="noopener noreferrer">
+					<button className="px-5 pt-3 pb-[14px] bg-orange-500 mt-14 rounded-lg text-[16px] w-fit h-fit">
+						Case Study coming soon
+					</button>
+				</a>
+			</div>
 
-				{/* Projects 4 */}
-				<div className="w-full px-6 py-5 text-black bg-white shadow-2xl dark:text-white dark:bg-darksecondary rounded-xl shadow-slate-200 dark:shadow-none">
-					<img
-						src={fixit}
-						alt="fixit"
-						className="relative object-cover rounded-t-lg"
-					/>
-					<h1 className="mt-6 text-[25px] font-semibold">
-						Travel Website
-					</h1>
-					<div className="w-10 h-[2px] mt-7 mb-9 bg-purple-400 rounded-full"></div>
-					<p className="mt-3 text-lg font-light">
-						This platform is where the users can find and recruit
-						professional service providers for their needs.
-					</p>
-					<a href="#" target="_blank" rel="noopener noreferrer">
-						<button className="px-5 pt-3 pb-[14px] dark:bg-darktertiary bg-lightbg font-medium mt-8 rounded-lg text-[16px] w-fit h-fit">
-							View Case Study
-						</button>
-					</a>
-				</div>
+			{/* Projects 3 */}
+			<div className="w-full px-8 pb-10 text-white bg-blue-700 mt-7 pt-9 rounded-xl bg-opacity-90">
+				<p className="text-[15px]">SWIFTBASKET</p>
+				<h1 className="text-5xl font-semibold mt-7">
+					Platform to fill customer's daily grocery needs
+				</h1>
+				<a href="#" target="_blank" rel="noopener noreferrer">
+					<button className="px-5 pt-3 pb-[14px] bg-blue-600 mt-14 rounded-lg text-[16px] w-fit h-fit">
+						Case Study coming soon
+					</button>
+				</a>
+			</div>
+
+			{/* Projects 4 */}
+			<div className="w-full px-8 pb-10 text-white bg-green-700 mt-7 pt-9 rounded-xl bg-opacity-90">
+				<p className="text-[15px]">E-COMMERCE BUILDER</p>
+				<h1 className="text-5xl font-semibold mt-7">
+					Building e-commerce and selling online
+				</h1>
+				<a href="#" target="_blank" rel="noopener noreferrer">
+					<button className="px-5 pt-3 pb-[14px] bg-green-600 mt-14 rounded-lg text-[16px] w-fit h-fit">
+						Case Study coming soon
+					</button>
+				</a>
 			</div>
 		</div>
 	);
