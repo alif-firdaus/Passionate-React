81397d26ecb9a359518b2170fc75e89a7dfe4a89
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-02 19:28:51 +0700

    Projects Section lg : updated

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -19,10 +19,10 @@ const Projects = () => {
 			</h1>
 
 			{/* Projects 1 */}
-			<div className="flex flex-col justify-between w-full px-8 mt-20 text-white bg-purple-700 lg:flex-row lg:mt-40 py-9 rounded-xl">
-				<div className="flex flex-col">
+			<div className="flex flex-col justify-between w-full px-8 mt-20 text-white bg-purple-700 lg:gap-8 lg:flex-row lg:mt-40 py-9 rounded-xl">
+				<div className="flex flex-col lg:w-[800px]">
 					<p className="text-[15px]">FIXIT MOBILE APP</p>
-					<h1 className="text-5xl font-semibold mt-7">
+					<h1 className="text-5xl font-semibold mt-7 lg:mt-10">
 						Connecting people with local service provider
 					</h1>
 					<a href="#" target="_blank" rel="noopener noreferrer">
@@ -41,10 +41,10 @@ const Projects = () => {
 			</div>
 
 			{/* Projects 2 */}
-			<div className="flex flex-col justify-between w-full px-8 text-white bg-orange-600 lg:flex-row py-9 mt-7 lg:mt-10 rounded-xl bg-opacity-90">
-				<div className="flex flex-col">
+			<div className="flex flex-col justify-between w-full px-8 text-white bg-orange-600 lg:gap-8 lg:flex-row py-9 mt-7 lg:mt-10 rounded-xl bg-opacity-90">
+				<div className="flex flex-col lg:w-[800px]">
 					<p className="text-[15px]">AQUA WALLET</p>
-					<h1 className="text-5xl font-semibold mt-7">
+					<h1 className="text-5xl font-semibold mt-7 lg:mt-10">
 						Transferring and storing Crypto and NFTs
 					</h1>
 					<a href="#" target="_blank" rel="noopener noreferrer">
@@ -63,10 +63,10 @@ const Projects = () => {
 			</div>
 
 			{/* Projects 3 */}
-			<div className="flex flex-col justify-between w-full px-8 text-white bg-blue-700 lg:flex-row py-9 mt-7 lg:mt-10 rounded-xl bg-opacity-90">
-				<div className="flex flex-col">
+			<div className="flex flex-col justify-between w-full px-8 text-white bg-blue-700 lg:gap-8 lg:flex-row py-9 mt-7 lg:mt-10 rounded-xl bg-opacity-90">
+				<div className="flex flex-col lg:w-[800px]">
 					<p className="text-[15px]">SWIFTBASKET</p>
-					<h1 className="text-5xl font-semibold mt-7">
+					<h1 className="text-5xl font-semibold mt-7 lg:mt-10">
 						Platform to fill customer's daily grocery needs
 					</h1>
 					<a href="#" target="_blank" rel="noopener noreferrer">
@@ -85,10 +85,10 @@ const Projects = () => {
 			</div>
 
 			{/* Projects 4 */}
-			<div className="flex flex-col justify-between w-full px-8 text-white bg-green-700 lg:flex-row py-9 mt-7 lg:mt-10 rounded-xl bg-opacity-90">
-				<div className="flex flex-col">
+			<div className="flex flex-col justify-between w-full px-8 text-white bg-green-700 lg:gap-8 lg:flex-row py-9 mt-7 lg:mt-10 rounded-xl bg-opacity-90">
+				<div className="flex flex-col lg:w-[800px]">
 					<p className="text-[15px]">E-COMMERCE BUILDER</p>
-					<h1 className="text-5xl font-semibold mt-7">
+					<h1 className="text-5xl font-semibold mt-7 lg:mt-10">
 						Building e-commerce and selling online
 					</h1>
 					<a href="#" target="_blank" rel="noopener noreferrer">
