9f9f1d5785022656c27f28dd3c0b262223bf9e13
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-02 19:33:02 +0700

    Getintouch Section lg : added

--- src/Pages/Getintouch.jsx
+++ src/Pages/Getintouch.jsx
@@ -4,10 +4,10 @@ const Getintouch = () => {
 	return (
 		<div
 			id="getintouch"
-			className="pt-20 pb-56 h-fit dark:bg-base bg-lightgrey px-sectionpxsm"
+			className="pt-20 pb-56 lg:pb-80 h-fit dark:bg-base bg-lightgrey px-sectionpxsm lg:px-sectionpxlg"
 		>
 			{/* Get in Touch Title */}
-			<h1 className="text-5xl font-semibold text-center text-black md:text-6xl dark:text-white">
+			<h1 className="text-5xl lg:text-[80px] font-semibold text-center text-black md:text-6xl dark:text-white">
 				Turning your ideas into{" "}
 				<span className="text-transparent bg-gradient-to-r dark:from-darkprimary from-lightprimary dark:via-purple-500 dark:to-pink-500 via-purple-500 to-pink-500 bg-clip-text">
 					real-life
@@ -16,7 +16,7 @@ const Getintouch = () => {
 			</h1>
 
 			{/* Main Button */}
-			<div className="flex justify-center mt-16 md:mt-16">
+			<div className="flex justify-center mt-16 md:mt-16 lg:mt-28">
 				<button className="pt-3 pb-[15px] text-[17px] font-medium dark:text-black text-white dark:bg-white bg-black rounded-xl w-fit h-fit px-10 dark:hover:bg-softgrey hover:bg-grey duration-300">
 					Let's talk!
 				</button>
