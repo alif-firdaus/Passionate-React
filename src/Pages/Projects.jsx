076461d808b579860c4d2fb42d4a3ccb51282e2e
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-02 18:59:19 +0700

    Projects Card : updated

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -5,9 +5,12 @@ import fixit from "../Assets/Images/Fixit App.png";
 
 const Projects = () => {
 	return (
-		<div id="projects" className="pb-44 pt-36 px-sectionpxsm">
+		<div
+			id="projects"
+			className="relative w-full h-auto pt-32 pb-40 lg:pt-52 lg:pb-64 px-sectionpxsm lg:px-sectionpxlg"
+		>
 			{/* Projects Title */}
-			<h1 className="text-5xl font-semibold text-center text-black md:text-6xl dark:text-white">
+			<h1 className="text-5xl lg:text-[80px] font-semibold text-center text-black md:text-6xl dark:text-white">
 				Featured projects I{" "}
 				<span className="text-transparent bg-gradient-to-r dark:from-darkprimary from-lightprimary dark:via-purple-500 dark:to-pink-500 via-purple-500 to-pink-500 bg-clip-text">
 					have created
@@ -16,16 +19,18 @@ const Projects = () => {
 			</h1>
 
 			{/* Projects 1 */}
-			<div className="w-full px-8 mt-20 text-white bg-purple-700 py-9 rounded-xl">
-				<p className="text-[15px]">FIXIT MOBILE APP</p>
-				<h1 className="text-5xl font-semibold mt-7">
-					Connecting people with local service provider
-				</h1>
-				<a href="#" target="_blank" rel="noopener noreferrer">
-					<button className="px-5 pt-3 pb-[14px] bg-purple-600 mt-14 rounded-lg text-[16px] w-fit h-fit">
-						Case Study coming soon
-					</button>
-				</a>
+			<div className="flex flex-col w-full px-8 mt-20 text-white bg-purple-700 lg:mt-40 py-9 rounded-xl">
+				<div className="flex flex-col">
+					<p className="text-[15px]">FIXIT MOBILE APP</p>
+					<h1 className="text-5xl font-semibold mt-7">
+						Connecting people with local service provider
+					</h1>
+					<a href="#" target="_blank" rel="noopener noreferrer">
+						<button className="px-5 pt-3 pb-[14px] bg-purple-600 mt-14 rounded-lg text-[16px] w-fit h-fit">
+							Case Study coming soon
+						</button>
+					</a>
+				</div>
 				<img
 					src={fixit}
 					alt="fixit"
@@ -34,16 +39,18 @@ const Projects = () => {
 			</div>
 
 			{/* Projects 2 */}
-			<div className="w-full px-8 text-white bg-orange-600 py-9 mt-7 rounded-xl bg-opacity-90">
-				<p className="text-[15px]">AQUA WALLET</p>
-				<h1 className="text-5xl font-semibold mt-7">
-					Transferring and storing Crypto and NFTs
-				</h1>
-				<a href="#" target="_blank" rel="noopener noreferrer">
-					<button className="px-5 pt-3 pb-[14px] bg-orange-500 mt-14 rounded-lg text-[16px] w-fit h-fit">
-						Case Study coming soon
-					</button>
-				</a>
+			<div className="flex flex-col w-full px-8 text-white bg-orange-600 py-9 mt-7 rounded-xl bg-opacity-90">
+				<div className="flex flex-col">
+					<p className="text-[15px]">AQUA WALLET</p>
+					<h1 className="text-5xl font-semibold mt-7">
+						Transferring and storing Crypto and NFTs
+					</h1>
+					<a href="#" target="_blank" rel="noopener noreferrer">
+						<button className="px-5 pt-3 pb-[14px] bg-orange-500 mt-14 rounded-lg text-[16px] w-fit h-fit">
+							Case Study coming soon
+						</button>
+					</a>
+				</div>
 				<img
 					src={fixit}
 					alt="fixit"
@@ -52,16 +59,18 @@ const Projects = () => {
 			</div>
 
 			{/* Projects 3 */}
-			<div className="w-full px-8 text-white bg-blue-700 py-9 mt-7 rounded-xl bg-opacity-90">
-				<p className="text-[15px]">SWIFTBASKET</p>
-				<h1 className="text-5xl font-semibold mt-7">
-					Platform to fill customer's daily grocery needs
-				</h1>
-				<a href="#" target="_blank" rel="noopener noreferrer">
-					<button className="px-5 pt-3 pb-[14px] bg-blue-600 mt-14 rounded-lg text-[16px] w-fit h-fit">
-						Case Study coming soon
-					</button>
-				</a>
+			<div className="flex flex-col w-full px-8 text-white bg-blue-700 py-9 mt-7 rounded-xl bg-opacity-90">
+				<div className="flex flex-col">
+					<p className="text-[15px]">SWIFTBASKET</p>
+					<h1 className="text-5xl font-semibold mt-7">
+						Platform to fill customer's daily grocery needs
+					</h1>
+					<a href="#" target="_blank" rel="noopener noreferrer">
+						<button className="px-5 pt-3 pb-[14px] bg-blue-600 mt-14 rounded-lg text-[16px] w-fit h-fit">
+							Case Study coming soon
+						</button>
+					</a>
+				</div>
 				<img
 					src={fixit}
 					alt="fixit"
@@ -70,16 +79,18 @@ const Projects = () => {
 			</div>
 
 			{/* Projects 4 */}
-			<div className="w-full px-8 text-white bg-green-700 py-9 mt-7 rounded-xl bg-opacity-90">
-				<p className="text-[15px]">E-COMMERCE BUILDER</p>
-				<h1 className="text-5xl font-semibold mt-7">
-					Building e-commerce and selling online
-				</h1>
-				<a href="#" target="_blank" rel="noopener noreferrer">
-					<button className="px-5 pt-3 pb-[14px] bg-green-600 mt-14 rounded-lg text-[16px] w-fit h-fit">
-						Case Study coming soon
-					</button>
-				</a>
+			<div className="flex flex-col w-full px-8 text-white bg-green-700 py-9 mt-7 rounded-xl bg-opacity-90">
+				<div className="flex flex-col">
+					<p className="text-[15px]">E-COMMERCE BUILDER</p>
+					<h1 className="text-5xl font-semibold mt-7">
+						Building e-commerce and selling online
+					</h1>
+					<a href="#" target="_blank" rel="noopener noreferrer">
+						<button className="px-5 pt-3 pb-[14px] bg-green-600 mt-14 rounded-lg text-[16px] w-fit h-fit">
+							Case Study coming soon
+						</button>
+					</a>
+				</div>
 				<img
 					src={fixit}
 					alt="fixit"
