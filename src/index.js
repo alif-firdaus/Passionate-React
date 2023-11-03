b3751b883c46defecadce089eadc2624a12e2171
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-03 23:33:00 +0700

    Projects Pages on index.js : added

--- src/index.js
+++ src/index.js
@@ -6,10 +6,32 @@ import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
 import App from "./App";
 
+// Projects
+import Fixit from "./Pages/Project/Fixit";
+import Aqua from "./Pages/Project/Aqua";
+import Swiftbasket from "./Pages/Project/Swiftbasket";
+import Ecommercebuilder from "./Pages/Project/Ecommercebuilder";
+
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
+	},
+	{
+		path: "/projects-fixit-mobile-app",
+		element: <Fixit />,
+	},
+	{
+		path: "/projects-aqua-wallet",
+		element: <Aqua />,
+	},
+	{
+		path: "/projects-swiftbasket",
+		element: <Swiftbasket />,
+	},
+	{
+		path: "/projects-ecommerce-builder",
+		element: <Ecommercebuilder />,
 	},
 ]);
 
