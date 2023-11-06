89d9f61636138049fc49474605d4b146bf24d29c
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-06 14:54:33 +0700

    Experience Section xl : added

--- src/Pages/Experience.jsx
+++ src/Pages/Experience.jsx
@@ -7,36 +7,36 @@ const Experience = () => {
 	return (
 		<div
 			id="experience"
-			className="relative w-full h-auto pt-32 pb-40 lg:pt-44 lg:pb-64 dark:bg-darksecondary bg-softblue px-sectionpxsm lg:px-sectionpxlg"
+			className="relative w-full h-auto pt-32 pb-40 lg:pt-44 lg:pb-64 dark:bg-darksecondary bg-softblue px-sectionpxsm lg:px-sectionpxlg xl:px-sectionpxxl"
 		>
 			{/* Experience Title */}
-			<h1 className="text-5xl font-semibold text-center dark:text-white text-black md:text-6xl lg:text-[70px]">
+			<h1 className="text-5xl xl:text-[80px] font-semibold text-center dark:text-white text-black md:text-6xl lg:text-[70px]">
 				My career path as{" "}
 				<span className="text-transparent bg-gradient-to-r dark:from-darkprimary from-lightprimary dark:via-purple-500 dark:to-pink-500 via-purple-500 to-pink-500 bg-clip-text">
 					Tech-savvy Artist
 				</span>{" "}
 			</h1>
 
-			<div className="flex flex-col items-center justify-between pt-20 lg:flex-row lg:pt-36 md:flex-row gap-7 lg:gap-5 md:gap-5 h-fit">
+			<div className="flex flex-col items-center justify-between pt-20 lg:flex-row lg:pt-36 md:flex-row gap-7 lg:gap-5 xl:gap-7 md:gap-5 h-fit">
 				{/* Experience 1 */}
 				<div className="w-full pb-8 text-black bg-white lg:pb-7 px-7 lg:px-5 pt-7 lg:pt-5 dark:text-white rounded-xl dark:bg-darktertiary">
 					<div className="flex items-center justify-start gap-5">
 						<div className="w-5 h-5 bg-purple-500 rounded lg:w-4 lg:h-4"></div>
 						<p className="text-xl font-medium lg:text-lg">JAN3</p>
 					</div>
-					<h1 className="text-[40px] lg:text-3xl font-medium mt-14 leading-tight">
+					<h1 className="text-[40px] lg:text-3xl xl:text-4xl font-medium mt-14 leading-tight">
 						UI/UX Designer
 					</h1>
-					<p className="text-lg font-light lg:text-[15px] mt-14">
+					<p className="text-lg font-light lg:text-[15px] xl:text-[17px] mt-14">
 						May 2023 - Present
 					</p>
 					<div className="items-center w-full pt-3 pb-[15px] mt-7 text-center rounded-lg dark:bg-white bg-black">
-						<p className="text-[17px] lg:text-[15px] font-medium text-white dark:text-black">
+						<p className="text-[17px] lg:text-[15px] xl:text-[17px] font-medium text-white dark:text-black">
 							United States
 						</p>
 					</div>
 					<div className="items-center w-full pt-3 pb-[15px] mt-4 text-center rounded-lg border-[1px] border-slate-300 dark:border-slate-500">
-						<p className="text-[17px] lg:text-[15px] font-medium dark:text-white text-black">
+						<p className="text-[17px] lg:text-[15px] xl:text-[17px] font-medium dark:text-white text-black">
 							Contract
 						</p>
 					</div>
@@ -44,7 +44,7 @@ const Experience = () => {
 						<div className="flex self-start pt-1">
 							<CheckCircleIcon className="w-[22px] lg:w-5 text-purple-500" />
 						</div>
-						<p className="text-lg lg:text-[15px] font-light">
+						<p className="text-lg lg:text-[15px] xl:text-[17px] font-light">
 							Improving and maintaining our Crypto E-wallet
 						</p>
 					</div>
@@ -52,7 +52,7 @@ const Experience = () => {
 						<div className="flex self-start pt-1">
 							<CheckCircleIcon className="w-[22px] lg:w-5 text-purple-500" />
 						</div>
-						<p className="text-lg lg:text-[15px] font-light">
+						<p className="text-lg lg:text-[15px] xl:text-[17px] font-light">
 							Developing and creating reusable UI assets
 						</p>
 					</div>
@@ -60,7 +60,7 @@ const Experience = () => {
 						<div className="flex self-start pt-1">
 							<CheckCircleIcon className="w-[22px] lg:w-5 text-purple-500" />
 						</div>
-						<p className="text-lg lg:text-[15px] font-light">
+						<p className="text-lg lg:text-[15px] xl:text-[17px] font-light">
 							Staying in the loop of the latest standards and
 							trends
 						</p>
@@ -75,19 +75,19 @@ const Experience = () => {
 							Ohceans
 						</p>
 					</div>
-					<h1 className="text-[40px] lg:text-3xl font-medium mt-14 leading-tight">
+					<h1 className="text-[40px] xl:text-4xl lg:text-3xl font-medium mt-14 leading-tight">
 						UI/UX Designer
 					</h1>
-					<p className="text-lg font-light lg:text-[15px] mt-14">
+					<p className="text-lg font-light xl:text-[17px] lg:text-[15px] mt-14">
 						Nov 2022 - May 2023
 					</p>
 					<div className="items-center w-full pt-3 pb-[15px] mt-7 text-center rounded-lg bg-white">
-						<p className="text-[17px] lg:text-[15px] font-medium text-black">
+						<p className="text-[17px] lg:text-[15px] xl:text-[17px] font-medium text-black">
 							Singapore
 						</p>
 					</div>
 					<div className="items-center w-full pt-3 pb-[15px] mt-4 text-center rounded-lg border-[1px] border-slate-400">
-						<p className="text-[17px] lg:text-[15px] font-medium text-white">
+						<p className="text-[17px] lg:text-[15px] xl:text-[17px] font-medium text-white">
 							Full-time
 						</p>
 					</div>
@@ -95,7 +95,7 @@ const Experience = () => {
 						<div className="flex self-start pt-1">
 							<CheckCircleIcon className="w-[22px] lg:w-5 text-white" />
 						</div>
-						<p className="text-lg lg:text-[15px] font-light">
+						<p className="text-lg lg:text-[15px] xl:text-[17px] font-light">
 							Developing user-flow, scenarios, and wireframe
 						</p>
 					</div>
@@ -103,7 +103,7 @@ const Experience = () => {
 						<div className="flex self-start pt-1">
 							<CheckCircleIcon className="w-[22px] lg:w-5 text-white" />
 						</div>
-						<p className="text-lg lg:text-[15px] font-light">
+						<p className="text-lg lg:text-[15px] xl:text-[17px] font-light">
 							Work with stakeholders to identify requirements
 						</p>
 					</div>
@@ -111,7 +111,7 @@ const Experience = () => {
 						<div className="flex self-start pt-1">
 							<CheckCircleIcon className="w-[22px] lg:w-5 text-white" />
 						</div>
-						<p className="text-lg lg:text-[15px] font-light">
+						<p className="text-lg lg:text-[15px] xl:text-[17px] font-light">
 							Aligning relevant features with the users' needs
 						</p>
 					</div>
@@ -125,19 +125,19 @@ const Experience = () => {
 							PetaniWeb
 						</p>
 					</div>
-					<h1 className="text-[40px] lg:text-3xl font-medium mt-14 leading-tight">
+					<h1 className="text-[40px] lg:text-3xl xl:text-4xl font-medium mt-14 leading-tight">
 						CPO & UI/UX
 					</h1>
-					<p className="text-lg lg:text-[15px] font-light mt-14">
+					<p className="text-lg lg:text-[15px] xl:text-[17px] font-light mt-14">
 						July 2020 - Present
 					</p>
 					<div className="items-center w-full pt-3 pb-[15px] mt-7 text-center rounded-lg dark:bg-white bg-black">
-						<p className="text-[17px] lg:text-[15px] font-medium text-white dark:text-black">
+						<p className="text-[17px] lg:text-[15px] xl:text-[17px] font-medium text-white dark:text-black">
 							Indonesia
 						</p>
 					</div>
 					<div className="items-center w-full pt-3 pb-[15px] mt-4 text-center rounded-lg border-[1px] border-slate-300 dark:border-slate-500">
-						<p className="text-[17px] lg:text-[15px] font-medium dark:text-white text-black">
+						<p className="text-[17px] lg:text-[15px] xl:text-[17px] font-medium dark:text-white text-black">
 							Self-employed
 						</p>
 					</div>
@@ -145,15 +145,15 @@ const Experience = () => {
 						<div className="flex self-start pt-1">
 							<CheckCircleIcon className="w-[22px] lg:w-5 text-blue-500" />
 						</div>
-						<p className="text-lg lg:text-[15px] font-light">
-							Building our MVP mobile app &#40;fixit.id&#41;
+						<p className="text-lg lg:text-[15px] xl:text-[17px] font-light">
+							Developing our MVP mobile app &#40;fixit.id&#41;
 						</p>
 					</div>
 					<div className="flex items-center justify-start gap-4 mt-4">
 						<div className="flex self-start pt-1">
 							<CheckCircleIcon className="w-[22px] lg:w-5 text-blue-500" />
 						</div>
-						<p className="text-lg lg:text-[15px] font-light">
+						<p className="text-lg lg:text-[15px] xl:text-[17px] font-light">
 							Definining strategy and identifying market
 							opportunities
 						</p>
@@ -162,7 +162,7 @@ const Experience = () => {
 						<div className="flex self-start pt-1">
 							<CheckCircleIcon className="w-[22px] lg:w-5 text-blue-500" />
 						</div>
-						<p className="text-lg lg:text-[15px] font-light">
+						<p className="text-lg lg:text-[15px] xl:text-[17px] font-light">
 							Aligning relevant features with the users' needs
 						</p>
 					</div>
