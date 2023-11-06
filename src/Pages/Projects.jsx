64c149d4f6e2ba600aea09e293ebe624572f5711
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-06 14:56:03 +0700

    projects Section xl : added

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -12,10 +12,10 @@ const Projects = () => {
 	return (
 		<div
 			id="projects"
-			className="relative w-full h-auto pt-32 pb-40 lg:pt-52 lg:pb-64 px-sectionpxsm lg:px-sectionpxlg"
+			className="relative w-full h-auto pt-32 pb-40 lg:pt-52 lg:pb-64 px-sectionpxsm lg:px-sectionpxlg xl:px-sectionpxxl"
 		>
 			{/* Projects Title */}
-			<h1 className="text-5xl lg:text-[70px] font-semibold text-center text-black md:text-6xl dark:text-white">
+			<h1 className="text-5xl lg:text-[70px] xl:text-[80px] font-semibold text-center text-black md:text-6xl dark:text-white">
 				Featured projects I{" "}
 				<span className="text-transparent bg-gradient-to-r dark:from-darkprimary from-lightprimary dark:via-purple-500 dark:to-pink-500 via-purple-500 to-pink-500 bg-clip-text">
 					have created
