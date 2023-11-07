b713c404ea58df789f1eac502a6332c77167e5fa
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-08 02:46:01 +0700

    About : updated

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+// Import Navbar
+import Navbar from "../Navbar";
+
 const About = () => {
-	return <div>About</div>;
+	return (
+		<div id="about" className="bg-lightbg dark:bg-darkbg">
+			<Navbar />
+			<div>About</div>
+		</div>
+	);
 };
 
 export default About;
